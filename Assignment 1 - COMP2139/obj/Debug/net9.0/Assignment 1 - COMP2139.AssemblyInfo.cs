56bebe7788ae988239b99d1f7fb291b88953f779
dev-mcknz <dev.mcknz@gmail.com>
2025-10-10 13:40:54 -0400

    pushign

--- Assignment 1 - COMP2139/obj/Debug/net9.0/Assignment 1 - COMP2139.AssemblyInfo.cs
+++ Assignment 1 - COMP2139/obj/Debug/net9.0/Assignment 1 - COMP2139.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment 1 - COMP2139")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc65fd5aa9a39c6aa5e053821392f2b846ae713d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a74de93ec732068ed70ad43764af5d42e874667")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment 1 - COMP2139")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment 1 - COMP2139")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
