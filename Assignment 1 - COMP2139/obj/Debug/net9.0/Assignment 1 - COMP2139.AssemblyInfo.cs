baf1dc281a21c9cd26ec161e05faf81ffacd4b06
dev-mcknz <dev.mcknz@gmail.com>
2025-10-15 00:11:57 -0400

    final

--- Assignment 1 - COMP2139/obj/Debug/net9.0/Assignment 1 - COMP2139.AssemblyInfo.cs
+++ Assignment 1 - COMP2139/obj/Debug/net9.0/Assignment 1 - COMP2139.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment 1 - COMP2139")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c95ffdb56c010f00877a8fff9cb616e75164b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+173bea886985473c43f9da901f1c9448f38675a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment 1 - COMP2139")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment 1 - COMP2139")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
